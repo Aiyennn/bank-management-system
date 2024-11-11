50f9103d56782c7c2aec5fc6b5e61bda48073544
Aiyen <146877043+Aiyennn@users.noreply.github.com>
2024-11-11 13:04:33 +0800

    user

--- src/com/bankmanagement/system/models/User.java
+++ src/com/bankmanagement/system/models/User.java
@@ -1,4 +1,38 @@
 package com.bankmanagement.system.models;
 
 public class User {
+    private String id;
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+    private String address;
+
+    public User(String id, String name, String phoneNumber, String emailAddress, String address) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.address = address;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
